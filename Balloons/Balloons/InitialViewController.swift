29e34fb697fb09842b71b743b9364633a8327b9b
Brena Amorim <brena.amorim@live.com>
2021-02-08 10:10:40 -0300

    initial setup

---
+++ Balloons/Balloons/InitialViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Balloons
+//
+//  Created by Brena Amorim on 08/02/21.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
